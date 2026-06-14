861ac2098da471425c9e68d0cfc400e02babd4b4
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-14 11:23:13 +0300

    src klasoru icindeki app.jsx component icinde mobil gorunum background photo eklendi, app.css dosyasinda css duzenlemesi yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import LightThemeMobileBackground from './assets/img/light-theme-mobile-background.png'
 import './App.css'
 
 export default function App() {
 
   return (
     <>
+    <div className="container">
+      <img 
+        src={LightThemeMobileBackground} 
+        className='light-theme-mobile-background' 
+        alt="" 
+      />
+    </div>
     </>
   )
 }
